f6294e3d696583d0b25118eac578a2c8ad3b9d75
Anh-Dung Phan <phananhdung309@yahoo.com>
2022-08-19 14:07:53 +0100

    Complete Hashmap exercises

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -31,15 +29,40 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
 
     for r in results.lines() {
         let v: Vec<&str> = r.split(',').collect();
-        let team_1_name = v[0].to_string();
+        let team_1_name = v[0];
         let team_1_score: u8 = v[2].parse().unwrap();
-        let team_2_name = v[1].to_string();
+        let team_2_name = v[1];
         let team_2_score: u8 = v[3].parse().unwrap();
-        // TODO: Populate the scores table with details extracted from the
-        // current line. Keep in mind that goals scored by team_1
-        // will be number of goals conceded from team_2, and similarly
-        // goals scored by team_2 will be the number of goals conceded by
-        // team_1.
+
+        match scores.get(team_1_name.into()) {
+            Some(current_team) => { 
+                let updated_team = Team { 
+                    goals_scored: current_team.goals_scored + team_1_score,
+                    goals_conceded: current_team.goals_conceded + team_2_score,
+                    name: team_1_name.into()
+                };
+                scores.insert(team_1_name.into(), updated_team);
+            },
+            None => {
+                let updated_team = Team { goals_scored: team_1_score, goals_conceded: team_2_score, name: team_1_name.into() };
+                scores.insert(team_1_name.into(), updated_team);
+            }
+        }
+        
+        match scores.get(team_2_name.into()) {
+            Some(current_team) => { 
+                let updated_team = Team { 
+                    goals_scored: current_team.goals_scored + team_2_score,
+                    goals_conceded: current_team.goals_conceded + team_1_score,
+                    name: team_2_name.into()
+                };
+                scores.insert(team_2_name.into(), updated_team);
+            },
+            None => {
+                let updated_team = Team { goals_scored: team_2_score, goals_conceded: team_1_score, name: team_2_name.into() };
+                scores.insert(team_2_name.into(), updated_team);
+            }
+        }
     }
     scores
 }
